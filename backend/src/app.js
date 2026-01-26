20bdec5d09c50963b36d5335eaa4b3cfd3f69406
Vedika-creates <vedikak1312@gmail.com>
2026-01-26 15:25:33 +0530

    deploymnet

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,17 +1,16 @@
 import express from "express";
 import cors from "cors";
 import routes from "./routes.js";
-import dotenv from "dotenv";
-dotenv.config();
-
-const PORT = process.env.PORT || 5000
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
+app.get("/", (req, res) => {
+  res.send("IMS Backend running 🚀");
+});
+
 app.use("/api", routes);
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
 export default app;
